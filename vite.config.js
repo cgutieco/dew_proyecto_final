40d7750c03f9ae30b4d970247d1b84cf37aa34dd
JAlexcc1 <jhonnypablo70@gmail.com>
2025-08-25 18:23:33 -0500

    Actualización de proyecto final: cambios recientes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/DEW-PROYECTO-FINAL/",
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/DEW-PROYECTO-FINAL/' : '/',
   plugins: [react()],
-})
+}))
