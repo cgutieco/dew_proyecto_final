b2e42d694398b549c569986a2e61de2bc864a2c1
JAlexcc1 <jhonnypablo70@gmail.com>
2025-08-25 18:54:45 -0500

    cambio4

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  base: '/DEW-PROYECTO-FINAL/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/DEW-PROYECTO-FINAL/' : '/',
   plugins: [react()],
-})
+}))
