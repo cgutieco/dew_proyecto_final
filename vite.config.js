0aca4e77856a8b2ce87510f8511209b7ba3ff5f5
cesar_gutierrez <cgutieco@outlook.com>
2025-08-29 15:13:40 -0500

    Fix: update base URL in vite.config.js for local deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => ({
-  base: "https://gcutieco.github.io/dew_proyecto_final",
+  base: "/dew_proyecto_final/",
   plugins: [react()],
 }))
