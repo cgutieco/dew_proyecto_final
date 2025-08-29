ce76cff760c3c249c83a57d1fe8162b341861779
JAlexcc1 <jhonnypablo70@gmail.com>
2025-08-29 18:13:18 -0500

    Add admin

--- src/data/users.js
+++ src/data/users.js
@@ -1,4 +1,4 @@
 export const usersDB = [
     {id: 1, email: "admin@example.com", password: "admin123", name: "Admin"},
-    {id: 2, email: "user@example.com", password: "user1234", name: "Usuario"},
+    {id: 2, email: "admin123@admin.com", password: "dadada", name: "Usuario"},
 ];
