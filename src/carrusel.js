4c81e6e64ed2d8bc36ef63c1e604935aab5a0c3c
cesar_gutierrez <cgutieco@outlook.com>
2025-08-29 15:02:30 -0500

    Refactor: update image imports in carrusel and datos

--- src/carrusel.js
+++ src/carrusel.js
@@ -1,23 +1,11 @@
-export const carrusel = [
-    {
-        id: 1,
-        nombreProducto: "imagen1",
-        urlImage: "/img/carrusel1.jpg",
-    },
-    {
-        id: 2,
-        nombreProducto: "imagen1",
-        urlImage: "/img/carrusel2.jpg",
-    },
-        {
-        id: 3,
-        nombreProducto: "imagen1",
-        urlImage: "/img/carrusel3.jpg",
-    },
-        {
-        id: 4,
-        nombreProducto: "imagen1",
-        urlImage: "/img/carrusel4.jpg",
-    }
+import carrusel1 from './assets/img/carrusel1.jpg'
+import carrusel2 from './assets/img/carrusel2.jpg'
+import carrusel3 from './assets/img/carrusel3.jpg'
+import carrusel4 from './assets/img/carrusel4.jpg'
 
+export const carrusel = [
+    {id: 1, nombreProducto: 'imagen1', urlImage: carrusel1},
+    {id: 2, nombreProducto: 'imagen1', urlImage: carrusel2},
+    {id: 3, nombreProducto: 'imagen1', urlImage: carrusel3},
+    {id: 4, nombreProducto: 'imagen1', urlImage: carrusel4}
 ]
--- src/datos.js
+++ src/datos.js
@@ -1,65 +1,21 @@
+import i5_12400 from './assets/imgProductos/i5-12400.jpg'
+import i5_14600k from './assets/imgProductos/i5-14600k.jpg'
+import i7_12700kf from './assets/imgProductos/i7-12700kf.jpg'
+import r3_3200g from './assets/imgProductos/r3-3200g.jpg'
+import r5_5600g from './assets/imgProductos/r5-5600g.jpg'
+import r5_8600g from './assets/imgProductos/r5-8600g.jpg'
+import r7_5700x from './assets/imgProductos/r7-5700x.jpg'
+import r7_8700g from './assets/imgProductos/r7-8700g.jpg'
+import ultra7_265k from './assets/imgProductos/ultra7-265k.jpg'
+
 export const datos = [
-    {
-        id: 1,
-        nombreProducto: "Intel core i5 12400",
-        urlImage: "/imgProductos/i5-12400.jpg",
-        precio: 200,
-        cantidad: 1
-    },
-        {
-        id: 2,
-        nombreProducto: "Intel core i5 14600k",
-        urlImage: "/imgProductos/i5-14600k.jpg",
-        precio: 300,
-        cantidad: 1
-    },
-        {
-        id: 3,
-        nombreProducto: "Intel core i7 12700kf",
-        urlImage: "/imgProductos/i7-12700kf.jpg",
-        precio: 400,
-        cantidad: 1
-    },
-        {
-        id: 4,
-        nombreProducto: "Ryzen 3 3200g",
-        urlImage: "/imgProductos/r3-3200g.jpg",
-        precio: 500,
-        cantidad: 1
-    },
-        {
-        id: 5,
-        nombreProducto: "Ryzen 5 5600g",
-        urlImage: "/imgProductos/r5-5600g.jpg",
-        precio: 600,
-        cantidad: 1
-    },
-        {
-        id: 6,
-        nombreProducto: "Ryzen 5 8600g",
-        urlImage: "/imgProductos/r5-8600g.jpg",
-        precio: 700,
-        cantidad: 1
-    },
-        {
-        id: 7,
-        nombreProducto: "Ryzen 7 5700x",
-        urlImage: "/imgProductos/r7-5700x.jpg",
-        precio: 800,
-        cantidad: 1
-    },
-        {
-        id: 8,
-        nombreProducto: "Ryzen 7 8700g",
-        urlImage: "/imgProductos/r7-8700g.jpg",
-        precio: 900,
-        cantidad: 1
-    },
-        {
-        id: 9,
-        nombreProducto: "Intel ultra 7 265k",
-        urlImage: "/imgProductos/ultra7-265k.jpg",
-        precio: 1000,
-        cantidad: 1
-    }
+    {id: 1, nombreProducto: 'Intel core i5 12400', urlImage: i5_12400, precio: 200, cantidad: 1},
+    {id: 2, nombreProducto: 'Intel core i5 14600k', urlImage: i5_14600k, precio: 300, cantidad: 1},
+    {id: 3, nombreProducto: 'Intel core i7 12700kf', urlImage: i7_12700kf, precio: 400, cantidad: 1},
+    {id: 4, nombreProducto: 'Ryzen 3 3200g', urlImage: r3_3200g, precio: 500, cantidad: 1},
+    {id: 5, nombreProducto: 'Ryzen 5 5600g', urlImage: r5_5600g, precio: 600, cantidad: 1},
+    {id: 6, nombreProducto: 'Ryzen 5 8600g', urlImage: r5_8600g, precio: 700, cantidad: 1},
+    {id: 7, nombreProducto: 'Ryzen 7 5700x', urlImage: r7_5700x, precio: 800, cantidad: 1},
+    {id: 8, nombreProducto: 'Ryzen 7 8700g', urlImage: r7_8700g, precio: 900, cantidad: 1},
+    {id: 9, nombreProducto: 'Intel ultra 7 265k', urlImage: ultra7_265k, precio: 1000, cantidad: 1}
 ]
